e6ab5094ee93d596955334bc8523414503b258ce
Max Lin <maxabcr2000@gmail.com>
2024-07-14 20:59:55 +0800

    Add keyboard input handling to move the spaceship entity

--- src/plugins/spaceship.rs
+++ src/plugins/spaceship.rs
@@ -3,16 +3,22 @@ use bevy::prelude::*;
 use super::{asset_loader::SceneAssets, movement::{Acceleration, MovingObjectBundle, Velocity}};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
-const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const SPACESHIP_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
 
 pub struct SpaceShipPlugin;
 impl Plugin for SpaceShipPlugin {
     fn build(&self, app: &mut App) {
         app
         //#NOTE: Need to be run after asset_loader plugin which is loaded at startup
-        .add_systems(PostStartup, spawn_entity);
+        .add_systems(PostStartup, spawn_entity)
+        .add_systems(Update, spaceship_movement_controls);
     }
 }
+
+#[derive(Component, Debug)]
+pub struct Spaceship;    //TAG
 
 // fn spawn_entity(mut commands: Commands) {
 //     commands.spawn((
@@ -22,8 +28,8 @@ impl Plugin for SpaceShipPlugin {
 // }
 
 fn spawn_entity(mut commands: Commands, scene_assets: Res<SceneAssets>) {
-    commands.spawn(MovingObjectBundle {
-        velocity: Velocity::new(STARTING_VELOCITY),
+    commands.spawn((MovingObjectBundle {
+        velocity: Velocity::new(Vec3::ZERO),
         acceleration: Acceleration::new(Vec3::ZERO),
         model: SceneBundle {
             //#NOTE: Models need to be located under the "assets" folder at the root level, not at the src level
@@ -31,5 +37,29 @@ fn spawn_entity(mut commands: Commands, scene_assets: Res<SceneAssets>) {
             transform: Transform::from_translation(STARTING_TRANSLATION),
             ..default()
         }
-    });
+    }, Spaceship));
+}
+
+fn spaceship_movement_controls(
+    mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>, 
+    //#NOTE: Bevy v0.12 Input => v0.13 ButtonInput
+    keyboard_input: Res<ButtonInput<KeyCode>>, 
+    time: Res<Time>,
+) {
+    let (mut transform, mut velocity) = query.single_mut();
+    let mut rotation = 0.0;
+    let mut roll = 0.0;
+    let mut movement = 0.0;
+
+    //#NOTE: Bevy v0.12 KeyCode::W => v0.13 KeyCode::KeyW
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        movement = SPACESHIP_SPEED;
+
+    //#NOTE: Bevy v0.12 KeyCode::S => v0.13 KeyCode::KeyS
+    } else if keyboard_input.pressed(KeyCode::KeyS) {
+        movement = -SPACESHIP_SPEED;
+    }
+
+    //#NOTE: It seems bevy's forward direction is opposite to the common model, so will need to use negative here
+    velocity.value = -transform.forward() * movement;
 }
